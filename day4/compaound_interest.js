ed42f3eacb7621d30a85a53824e17deb759fd26e
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-20 14:52:39 -0500

    ende of class 8/20/2020

--- day4/compaound_interest.js
+++ day4/compaound_interest.js
@@ -1,13 +1,17 @@
 const prompt = require("prompt-sync")();
-let pv=prompt("enter persent total value")
-pv=parseInt(pv);
-let rate=prompt("enter the interest rate ")
-rate=parseInt(pv);
-let pv=prompt("enter persent total value")
-pv=parseInt(pv);
-let year=prompt("for how many year ")
+let pv=prompt("enter persent total value:")
+pv=parseFloat(pv);
+let rate=prompt("enter the interest rate: ")
+rate=parseFloat(pv);
+let year=prompt("for how many year: ")
 year=parseFloat(year)
+let percentrate=(pv*rate)/100;
+let interest= 1+percentrate;
+let step=Math.pow(interest,year)
+// Math.pow((1+(rate/100)),year);
+step=step-1;
 
-let fv=pv(Math.pow((1+rate),year))
+let fv=pv*step;
 
-console.log("your interest ")
+
+console.log("your interest for"+" "+year+"year is ="+fv)
