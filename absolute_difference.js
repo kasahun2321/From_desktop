d76b38bcaae53afd2784cd60cce468405edafa0a
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-20 01:46:38 -0500

    weather and shoes

---
+++ absolute_difference.js
@@ -0,0 +1,16 @@
+
+
+z=x-y
+
+if ( z<0)
+{
+    z=z*-1;
+    console.log(`input x  input y  the abs value difference`)
+    console.log(x+"\t" +"  "+y+"\t"+"   "+z);
+
+}
+else
+{
+    console.log(`input x   input y  the abs value difference`)
+    console.log(x+"\t"+y+"\t"+z);
+}
---
+++ waether_foot_wear.js
@@ -0,0 +1,21 @@
+let userinput=require('prompt-sync')();
+
+let x =userinput("Enter the weather   :");
+
+
+let wear;
+
+if(x==="hot")
+{
+wear="sandals"
+}
+else if(x==="rain")
+{
+   wear="galoshes "
+}
+else if(x==="snow")
+{
+    wear="boots"
+}
+console.log("weather   wearing style")
+console.log(  x+"\t"+"  "+wear)
