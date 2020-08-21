c39d0ef8750681c1f33aa9990913e106d23ff75d
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-21 10:14:07 -0500

    chapter8

---
+++ day4/q3_all_n_pring.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+let user_input=prompt("Enter number of iteration ")
+let n=parseInt(user_input)
+let i=0,all;
+while(i<=n)
+{
+all=i;
+i++
+console.log(all);
+}
+
+
