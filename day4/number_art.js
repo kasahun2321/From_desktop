e446117046e436c0374b370192ba74d758693fb8
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-21 00:35:31 -0500

    community page

--- day4/number_art.js
+++ day4/number_art.js
@@ -1,34 +1,48 @@
 
-let i,j,z;
+let i,j;
+let z=[];
 for (i=1; i<=5;i++)
 {
-    for(j=0;j<=5;j++)
+    for(j=0;j<5;j++)
     {
-      z=i;  
-      console.log(z);
+      z[j]=i;  
+      //console.log("line"+i+"/"+z[j]+"/t");
+      console.log(z[j])
     }
+
 }
 
 
-//////////// printing 12345 in horizoantal
-//let i,j,z;
-for (i=1; i<=5;i++)
-{
-    for(j=1;j<=5;j++)
-    {
-      z=j;  
-      console.log(z+"\t");
-    }
+// //////////// printing 12345 in horizoantal
+// //let i,j,z;
+// for (i=1; i<=5;i++)
+// {
+//     for(j=1;j<=5;j++)
+//     {
+//       z=j;  
+//       console.log(z+"\t");
+//     }
     
-}
+// }
 
-///triangle number
-for (i=1; i<=5;i++)
-{
-    for(j=1;j<=i;j++)
-    {
-      z=j;  
-      console.log(z+"\t"+z);
-    }
-    console.log("\t");
-}
+// ///triangle number
+// for (i=1; i<=5;i++)
+// {
+//     for(j=1;j<=i;j++)
+//     {
+//       z=j;  
+//       console.log(z+"\t");
+//     }
+//     //console.log("\t");
+// }
+
+// ///
+// for (i=5; i>=1;i--)
+// {
+//     for(j=1;j<=i;j++)
+//     {
+//       z=i;  
+//       console.log(z+"\t");
+//     }
+    
+// }
---
+++ rochester_community_of_Ethiopia/index.js
@@ -0,0 +1,9 @@
+
+function display()
+{
+  document.getElementById('selam').innerHTML="habeshe belome the beshasha";
+}
+function helo()
+{
+  alert("hello java script");
+}
