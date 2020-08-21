cddf54beaf24a7b46e1efe7019217af51e5638fd
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-21 17:44:55 -0500

    day5

--- day4/number_art.js
+++ day4/number_art.js
@@ -1,48 +1,119 @@
 
+const input = require("prompt-sync")();
 let i,j;
-let z=[];
+
 for (i=1; i<=5;i++)
 {
+  let z="";
     for(j=0;j<5;j++)
     {
-      z[j]=i;  
-      //console.log("line"+i+"/"+z[j]+"/t");
-      console.log(z[j])
+      z+=i+""; 
+      //console.log(z); 
+    
+      
     }
+    console.log(z)
+ }
+//traingle star 
+let limit='';
+let user=input("what is the max star");
+user=parseInt(user)
+
+for (let i=user;limit>-1;limit--)
+{
+  let result="";
+  for(let j=i;j>=1;j--)
+  {
+    result +="*";
+    console.log(result);
+  }
 
 }
 
+//////////// printing 12345 in horizoantal
 
-// //////////// printing 12345 in horizoantal
-// //let i,j,z;
-// for (i=1; i<=5;i++)
-// {
-//     for(j=1;j<=5;j++)
-//     {
-//       z=j;  
-//       console.log(z+"\t");
-//     }
+for (i=1; i<=5;i++)
+{
+  let z=1;
+    for(j=1;j<=5;j++)
+    {
+      z+=j+"";  
+      
+    }
+    console.log(z+"\t"); 
+}
+
+///triangle number
+for (i=1; i<=5;i++)
+{
+  let z="";
+    for(j=1;j<=i;j++)
+    {
+      z+=j+"";  
+      
+    }
+    console.log(z+"\t");
+}
+
+// /// very excepetional pattern.
+/*5
+55
+555
+4
+44
+444
+3
+33
+333
+.....*/
+for (i=5; i>=1;i--)
+{
+  let z="";
+    for(j=1;j<=i;j++)
+    {
+      z+=i+"";  
+      console.log(z+"\t");
+    }
     
-// }
+}
+//////
+/*
+1
+12
+123
+1234
+12345
+*/
+for (i=1; i<=5;i++)
+{
+  let z="";
+    for(j=1;j<=i;j++)
+    {
+      z+=i+"";  
+      
+    }
+    console.log(z+"\t"); 
+}
 
-// ///triangle number
-// for (i=1; i<=5;i++)
-// {
-//     for(j=1;j<=i;j++)
-//     {
-//       z=j;  
-//       console.log(z+"\t");
-//     }
-//     //console.log("\t");
-// }
+// amazing pattern
 
-// ///
-// for (i=5; i>=1;i--)
-// {
-//     for(j=1;j<=i;j++)
-//     {
-//       z=i;  
-//       console.log(z+"\t");
-//     }
-    
-// }
+
+/*l
+last pattern
+55555
+4444
+333
+22
+1
+*/
+
+for (i=5; i>=1;i--)
+{
+  let z="";
+    for(j=1;j<=i;j++)
+    {
+      z+=i+"";  
+      
+    }
+    console.log(z+"\t"); 
+}
---
+++ day5/vowel_checker_dani.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+let user_input=prompt("please enter string")
+
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count(user_input));
