992898d786a9d7a9c2addf6cff5f63e1f865c60e
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-22 14:29:51 -0500

    vowel_count

--- day5/test.js
+++ day5/test.js
@@ -1,1 +1,87 @@
-consol
+// let x = 1;
+// let y = '2';
+// let z = true;
+// let s = x + y;
+// console.log(s) // output? 12
+// console.log(typeof s) // output?string
+// x = x + z;
+// console.log(x) // output?2
+// console.log(typeof x) // output? number
+// y = x++ * y;
+// console.log(x); // output?3
+// console.log(typeof x); // output? number
+// s = parseInt(s)
+// s+='3';
+// console.log(s); // output? 123
+// console.log(typeof s); // output? string
+// z= !z;
+// console.log(z); // output? flase
+// console.log(typeof z); // output? boolean
+
+// let x = 0;
+//  x++;
+//  console.log(x);//x =1
+//  ++x;
+//  console.log(x);//x=2
+//  console.log(++x);//x=3
+//  console.log(x);//x=3
+//  console.log(x++);//x=3
+//  console.log(x);//x=4
+
+
+
+// let x = 5;
+// let y = 7+'';
+// console.log(x+y);
+
+// let x= 0;
+//  let y = "hello";
+//  let z = x || "Hi";
+//  console.log(z);
+//  let w = y || 5;
+//  console.log(w);
+
+//  for(let count =1; count<=10; i++){// error code 
+//     console.log(count);
+//    }
+//    console.log(count); // value of count here?
+
+// const prompt = require("prompt-sync")();
+
+// let num;
+
+// for(let x=10;x<100;x++)
+// {
+//  if (x%3===0 && x%5===0)
+//  {
+//      x=x;
+//      console.log(x)
+//  }
+ 
+// }
+// let x = 1, y = 7;
+// let message = "Hi";
+// if (x < 2) {
+//  if (y > 7) {
+//  message = "Bye";
+//  }else{
+// message = "HOla";
+// }
+// } else {
+//  message = "Hello";
+// }
+// console.log(message);
+
+
+// for(let x=0;x<3;x++)
+// {
+//     console.log(x)
+// } 
+// console.log(x)//out of scope variable
+
+let d=0;
+while(d<3)
+{
+    console.log(d)
+    d++;
+}
--- day5/vowel_checker_dani.js
+++ day5/vowel_checker_dani.js
@@ -3,7 +3,7 @@ let user_input=prompt("please enter string")
 
 function vowel_count(str1)
 {
-  var vowel_list = 'aeiouAEIOU';
+  var vowel_list = 'aA';
   var vcount = 0;
   
   for(var x = 0; x < str1.length ; x++)
@@ -17,3 +17,4 @@ function vowel_count(str1)
   return vcount;
 }
 console.log(vowel_count(user_input));
+//document.write("document .wright exalmpe")
