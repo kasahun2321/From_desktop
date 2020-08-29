4d932f0c9766033e0a332bbc7d6bb46a0477dd7c
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-08-29 01:50:05 -0500

    slite change

--- week2/week2_day4/assignment/Q1.js
+++ week2/week2_day4/assignment/Q1.js
@@ -1,7 +1,12 @@
+"use strict"
 
 function ExtractGivenName(fullname)
 {
+let name=fullname.split(",")
+return name[0];
 
 }
 
-const personal_data={name:kasahun,agge:27,degre:msd,heigth:1.68};
+const personal_data="Kasahun, Tehone, Belaye";
+let given_name=ExtractGivenName(personal_data);
+console.log("Given name :"+given_name)
--- week2/week2_day4/assignment/Q2.js
+++ week2/week2_day4/assignment/Q2.js
@@ -1,7 +1,28 @@
-function prefix()
+"use strict"
+function prefix(swimming,walking)
 {
     /// from the list of array then deretmine the string which have same string prefix dis
+let x=swimming.split("");
+let y=walking.split("")
+let o=x.length-3;
+let p=y.length-3;
+let result=[];
+for(let i=o;i<o+3;o++)
+{    result=[];
+    if(x[o]==y[p])
+    {
+        result[o]=x[o];
+        p++;
+    }
+}
+
+return result;
 
 }
 
 let list=["disable","distateful"];
+let xx="walking";
+let yy="swimming";
+
+let z=prefix(xx,yy);
+console.log(z);
